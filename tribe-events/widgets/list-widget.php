b1de9c85977c8668f73cadb90bf27b0d5caba93c
Owen Mundy <omundy@gmail.com>
2019-02-01 07:53:45 -0500

    Last commit from fall 2018 calendar team

--- tribe-events/widgets/list-widget.php
+++ tribe-events/widgets/list-widget.php
@@ -35,97 +35,62 @@ if ( $posts ) : ?>
 
 	<ol class="tribe-list-widget">
 		<?php
-		// Setup the post data for each event.
-		foreach ( $posts as $post ) :
-			setup_postdata( $post );
-			?>
-			<li class="tribe-events-list-widget-events <?php tribe_events_event_classes() ?>">
-				<?php
-				if (
-					tribe( 'tec.featured_events' )->is_featured( get_the_ID() )
-					&& get_post_thumbnail_id( $post )
-				) {
-					/**
-					 * Fire an action before the list widget featured image
-					 */
-					do_action( 'tribe_events_list_widget_before_the_event_image' );
+			// Setup the post data for each event.
+			foreach ( $posts as $post ) :
+				setup_postdata( $post );
+		?>
+		<li class="tribe-events-list-widget-events <?php tribe_events_event_classes() ?>">
 
-					/**
-					 * Allow the default post thumbnail size to be filtered
-					 *
-					 * @param $size
-					 */
-					$thumbnail_size = apply_filters( 'tribe_events_list_widget_thumbnail_size', 'post-thumbnail' );
+			<?php do_action( 'tribe_events_list_widget_before_the_event_title' ); ?>
 
-					/**
-					 * Filters whether the featured image link should be added to the Events List Widget
-					 *
-					 * @since 4.5.13
-					 *
-					 * @param bool $featured_image_link Whether the featured image link should be added or not
-					 */
-					$featured_image_link = apply_filters( 'tribe_events_list_widget_featured_image_link', true );
-					$post_thumbnail      = get_the_post_thumbnail( null, $thumbnail_size );
-
-					if ( $featured_image_link ) {
-						$post_thumbnail = '<a href="' . esc_url( tribe_get_event_link() ) . '">' . $post_thumbnail . '</a>';
-					}
-					?>
-					<div class="tribe-event-image">
-						<?php
-						// not escaped because it contains markup
-						echo $post_thumbnail;
-						?>
+			<!-- BOOTSTRAP SNIPPET | EVENT INFO SIDEBAR AND FOOTER SETUP-->
+			<div class="container tribe-events-info-block">
+				<div class="row">
+					<div class="col-3">
+						<div class="block-date">
+							<span class="event-month"><?php echo tribe_get_start_date ( $post, $display_time = false, $date_format = d );  ?></span>
+							<span class="event-day"><?php echo tribe_get_start_date ( $post, $display_time = false, $date_format = M );  ?></span>
+						</div>
 					</div>
-					<?php
+					<div class="col-9">
+						<div class="event-info">
 
-					/**
-					 * Fire an action after the list widget featured image
-					 */
-					do_action( 'tribe_events_list_widget_after_the_event_image' );
-				}
-				?>
+							<!-- Event Title -->
+							<h4 class="tribe-event-title">
+								<a href="<?php echo esc_url( tribe_get_event_link() ); ?>" rel="bookmark"><?php the_title(); ?></a>
+							</h4>
 
-				<?php do_action( 'tribe_events_list_widget_before_the_event_title' ); ?>
+							<?php do_action( 'tribe_events_list_widget_after_the_event_title' ); ?>
 
+							<?php do_action( 'tribe_events_list_widget_before_the_meta' ) ?>
 
+							<!-- Event Time -->
+							<span class="time"><?php echo tribe_get_start_time ( $event_id, 'g:i A'); ?></span>
 
-<div class="block-date">
-	<span class="month"><?php echo tribe_get_start_date ( $post, $display_time = false, $date_format = M );  ?></span>
-	<span class="day">  <?php echo tribe_get_start_date ( $post, $display_time = false, $date_format = d );  ?></span>
-</div>
+							<!-- Event description (which adds the 'read more' button) -->
+							<?php the_excerpt(); ?>
+						</div>
+					</div>
+				</div>
+			</div>
 
-<div class="event-info">	
+			<!-- <div class="tribe-event-duration">
+				<?php echo tribe_events_event_schedule_details(); ?>
+			</div> -->
 
-				<!-- Event Title -->
-				<h4 class="tribe-event-title">
-					<a href="<?php echo esc_url( tribe_get_event_link() ); ?>" rel="bookmark"><?php the_title(); ?></a>
-				</h4>
+			<?php do_action( 'tribe_events_list_widget_after_the_meta' ) ?>
 
-				<?php do_action( 'tribe_events_list_widget_after_the_event_title' ); ?>
-				<!-- Event Time -->
-
-				<?php do_action( 'tribe_events_list_widget_before_the_meta' ) ?>
-
-				
-
-	<span class="time"><?php echo tribe_get_start_time ( $event_id, 'g:i A'); ?></span>
-</div>
-
-
-				<!-- <div class="tribe-event-duration">
-					<?php echo tribe_events_event_schedule_details(); ?>
-				</div> -->
-
-				<?php do_action( 'tribe_events_list_widget_after_the_meta' ) ?>
-			</li>
+			<!-- here we're trying a regular expression to find and replace 
+			the big annoying 'read more' button -->
+			<?php $map = preg_replace('/<a class="btn btn-secondary understrap-read-more-link"[^>]*>.*?</a>/s','',$map); ?>
+		</li>
 		<?php
 		endforeach;
 		?>
 	</ol><!-- .tribe-list-widget -->
 
 	<p class="tribe-events-widget-link">
-		<a href="<?php echo esc_url( tribe_get_events_link() ); ?>" rel="bookmark"><?php printf( esc_html__( 'View All %s', 'the-events-calendar' ), $events_label_plural ); ?></a>
+		<a href="<?php echo esc_url( tribe_get_events_link() ); ?>" rel="bookmark"><?php printf( esc_html__( 'See All %s', 'the-events-calendar' ), $events_label_plural ); ?></a>
 	</p>
 
 <?php
