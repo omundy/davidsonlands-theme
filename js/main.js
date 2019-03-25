d27aae79412e6b066ae188551e255e2328d2edf7
Owen Mundy <omundy@gmail.com>
2019-03-25 09:54:54 -0400

    Fix submenu on mobile

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
 
 
-	$(".headerOpenSearch").on("click" , function() {
+	$(".headerOpenSearch").on("click", function() {
 		if ($(".headerSearchDiv").css("display") == "none") {
 			$(".headerSearchDiv").css("display", "block");
 			$(".headerSearchDiv input").focus();
@@ -21,9 +21,9 @@ $(document).ready(function() {
 		//console.log(e.target);
 		if (!$(e.target).hasClass('headerSearchIcon') && !$(e.target).hasClass('headerOpenSearch') &&
 			!$(e.target).hasClass('header-search-btn') && !$(e.target).hasClass('headerSearchForm') &&
-			!$(e.target).hasClass('headerSearchFormInput')){
-				hideHeaderSearch();
-			}
+			!$(e.target).hasClass('headerSearchFormInput')) {
+			hideHeaderSearch();
+		}
 	});
 
 	function showHeaderSearch() {}
@@ -31,5 +31,20 @@ $(document).ready(function() {
 	function hideHeaderSearch() {
 		$(".headerSearchDiv").fadeOut("2000");
 	}
+
+
+
+	// add dropdown toggle to submenu on mobile
+	$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+		// Avoid following the href location when clicking
+		event.preventDefault();
+		// Avoid having the menu to close when clicking
+		event.stopPropagation();
+		// If a menu is already open we close it
+		$('ul.dropdown-menu [data-toggle=dropdown]').parent().removeClass('open');
+		// opening the one you clicked on
+		$(this).parent().addClass('open');
+	});
+
 
 });
