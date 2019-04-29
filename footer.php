6c69312ef8e1fe71784c2cc163b890b98d864bc9
Owen Mundy <omundy@gmail.com>
2019-04-29 13:40:29 -0400

    Update LTA logo

--- footer.php
+++ footer.php
@@ -123,30 +123,30 @@ $container = get_theme_mod('understrap_container_type');
 
 
 		<div class="row">
-			<div class="col-3 mt-5">
+			<div class="col-6 col-md-3 mt-5">
 
 				<p>
 					<a rel="home" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" itemprop="url">
-						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo-tan.svg" alt="Davidson Lands Conservancy logo" class="header-logo">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo-tan.svg" alt="Davidson Lands Conservancy logo" class="img-fluid">
 					</a>
 				</p>
-
+PO Box 1952, Davidson, NC 28036<br>704-892-1910 | <a href="<?php echo esc_url(home_url('/')); ?>about/contact-us/">Contact us</a>
 			</div>
-			<div class="col-6 mt-5">
+			<div class="col-12 col-md-7 mt-5">
 
 
 
-<p>© <?php echo date("Y"); ?> Davidson Lands Conservancy, PO Box 1952, Davidson, North Carolina 28036 | 704-892-1910 | <a href="<?php echo esc_url(home_url('/')); ?>about/contact-us/">Contact us</a></p>
+<p>© <?php echo date("Y"); ?> Davidson Lands Conservancy</p>
 
 <p>Davidson Lands Conservancy is a 501 ( c ) ( 3 ) organization holding NC Charitable Solicitation License #SLOO4342. <a href="<?php echo esc_url(home_url('/')); ?>">Read our most recent Reviewed Financial Statements</a>.</p>
 
 <p>Website designed and developed by <a href="https://owenmundy.com/site/critical-web-design" target="_blank">Critical Web Design</a> at Davidson College</p>
 
 			</div>
-			<div class="col-3 mt-5">
+			<div class="col-6 col-md-2 mt-5">
 
 				<p>
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/lta_seal_white.svg" alt="LTA logo" class="header-logo">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/lta_seal_white.svg" alt="LTA logo" class="img-fluid" style="max-width:85%">
 				</p>
 
 			</div>
