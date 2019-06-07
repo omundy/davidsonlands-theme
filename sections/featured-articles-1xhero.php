3584f666b7cb085919ece79bdaff152ca1cf0c39
Owen Mundy <omundy@gmail.com>
2019-06-07 11:51:25 -0700

    Added if-else statement to get text from excerpt or content - OC

--- sections/featured-articles-1xhero.php
+++ sections/featured-articles-1xhero.php
@@ -35,8 +35,13 @@ if(count($recent_posts) > 0):
             <div class="col-8">
 
             <?php echo '<h3><a href="' . get_permalink($recent['ID']) . '">'. $recent['post_title'] .'</a></h3>'; ?>
-            <?php echo '<p>'. sentenceTrim($recent['post_excerpt']) .'</p>'; ?>
-
+            <?php
+                if ($recent['post_excerpt'] == '') {
+                    echo '<p>'. sentenceTrim($recent['post_content']) .'</p>';
+                } else {
+                    echo '<p>'. sentenceTrim($recent['post_excerpt']) .'</p>';
+                }
+            ?>
             <?php echo '<a class="btn btn-info" href="' . get_permalink($recent['ID']) . '">CONTINUE READING</a>'; ?>
             <?php echo '<a class="btn btn-info" href="' . esc_url( home_url() ) . '/category/news">MORE NEWS</a>'; ?>
 
