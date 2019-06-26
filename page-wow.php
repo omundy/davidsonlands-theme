aecac838e3a823168a759d83f4b7ed140ac59249
Owen Mundy <omundy@gmail.com>
2019-06-26 10:54:01 -0700

    Added a new .php file for WOW page - OC

---
+++ page-wow.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Join Us > WOW
+ * Template Post Type: post, page, event
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+get_header();
+$container   = get_theme_mod( 'understrap_container_type' );
+?>
+
+
+<?php get_footer(); ?>
